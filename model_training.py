1a1c15ff7a7dbc99411f006297db0322f00a0074
Neha Bhavsar <146441745+BhavsarNeha@users.noreply.github.com>
2024-06-21 17:54:34 +0530

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 import joblib
 
@@ -6,3 +7,8 @@ def train_model(X_train, y_train):
     model.fit(X_train, y_train)
     joblib.dump(model, 'model.pkl')
     return model
+
+if __name__ == "__main__":
+    X_train = pd.read_csv('data/X_train.csv')
+    y_train = pd.read_csv('data/y_train.csv')
+    train_model(X_train, y_train)
